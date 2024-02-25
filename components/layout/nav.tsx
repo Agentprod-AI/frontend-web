0529321ee5ff3a057e0db3a602e40b173dc4adb8
Naman <naman.barkiya02@gmail.com>
2024-02-26 00:55:11 +0530

    Mobile: sidenav for mobile and width initialization fix

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -28,7 +28,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
   return (
     <div
       data-collapsed={isCollapsed}
-      className="pt-16 group flex-col gap-4 flex"
+      className="md:pt-16 group flex-col gap-4 flex"
     >
       <nav className="grid gap-1 px-2 pt-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
         {/* <Separator /> */}
