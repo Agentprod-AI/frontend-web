1eee5c58ca13bf6ab7cbbabe939eebacef470a16
Naman <naman.barkiya02@gmail.com>
2024-02-29 17:29:34 +0530

    Bug fix: UseEffect infinite problem campaign page

--- app/(dashboard)/dashboard/campaign/page.tsx
+++ app/(dashboard)/dashboard/campaign/page.tsx
@@ -6,53 +6,59 @@ import {
   Popover,
   PopoverContent,
   PopoverTrigger,
-} from "@/components/ui/popover"
-import {
-  Card,
-  CardTitle,
-  CardContent
-} from "@/components/ui/card";
+} from "@/components/ui/popover";
+import { Card, CardTitle, CardContent } from "@/components/ui/card";
 import Link from "next/link";
 import { useCampaignContext } from "@/context/campaign-provider";
 import { DummyCampaign } from "@/constants/campaign";
 
 export default function Page() {
-  const { campaign, updateCampaignState, toggleCampaignEnabled, activeCampaignId, setActiveCampaignId} = useCampaignContext();
+  const {
+    campaign,
+    updateCampaignState,
+    toggleCampaignEnabled,
+    activeCampaignId,
+    setActiveCampaignId,
+  } = useCampaignContext();
 
   function formatKey(key: any) {
     return key
-      .replace(/([A-Z])/g, ' $1')
-      .replace(/^./, function(str: string) { return str.toUpperCase(); })
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, function (str: string) {
+        return str.toUpperCase();
+      })
       .trim();
   }
 
   useEffect(() => {
     updateCampaignState({ campaign: DummyCampaign });
-  }, [updateCampaignState]);
+  }, []);
 
   return (
     <div className="space-y-2">
       <Card className="bg-accent px-4 py-6">
-        <CardTitle>
-          Send your first outbound Email campaign
-        </CardTitle>
+        <CardTitle>Send your first outbound Email campaign</CardTitle>
         <Button className="mt-4"> + Create Campaign </Button>
       </Card>
 
-      {campaign && campaign.map((campaignItem) => (
-        <Card key={campaignItem.campaignId}> 
-          <CardContent className="flex items-center justify-between space-x-4 p-4 gap-4">
-            <div className="flex space gap-3">
-            <Switch 
-              checked={campaignItem?.isEnabled}
-              onCheckedChange={() => campaignItem.campaignId !== undefined && toggleCampaignEnabled(campaignItem.campaignId)}
-            />
-              <p className="text-sm font-medium leading-none w-[150px]">
-                {campaignItem?.campaignName}
-              </p>
-            </div>
+      {campaign &&
+        campaign.map((campaignItem) => (
+          <Card key={campaignItem.campaignId}>
+            <CardContent className="flex items-center justify-between space-x-4 p-4 gap-4">
+              <div className="flex space gap-3">
+                <Switch
+                  checked={campaignItem?.isEnabled}
+                  onCheckedChange={() =>
+                    campaignItem.campaignId !== undefined &&
+                    toggleCampaignEnabled(campaignItem.campaignId)
+                  }
+                />
+                <p className="text-sm font-medium leading-none w-[150px]">
+                  {campaignItem?.campaignName}
+                </p>
+              </div>
 
-            {/* <div className="flex justify-between">
+              {/* <div className="flex justify-between">
               {campaignItem.analytics && Object.entries(campaignItem.analytics).map(([key, value]) => (
                 <div key={key} style={{ margin: '0 10px', textAlign: 'center' }}>
                   <p className="text-xs text-muted-foreground">
@@ -65,39 +71,43 @@ export default function Page() {
               ))}
             </div> */}
 
-            {/* <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-2 h-6"></div> */}
+              {/* <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mx-2 h-6"></div> */}
 
-            <Link href={"/dashboard"}>
-              {/* <Button 
-              variant={"outline"}
-              onClick={() => setActiveCampaignId(campaignItem.campaignId)}> */}
-                Analytics
-              {/* </Button> */}
-            </Link>
-            
+              <Button
+                variant={"outline"}
+                asChild
+                onClick={() => setActiveCampaignId(campaignItem.campaignId)}
+              >
+                <Link href={"/dashboard"}>Analytics</Link>
+              </Button>
 
-            <div className="flex">
-              <p className="text-xs text-muted-foreground w-14">
-                {new Date(campaignItem.createdAt).toLocaleTimeString([], { hour: '2-digit', minute: '2-digit', hour12: true })}
-              </p>
+              <div className="flex">
+                <p className="text-xs text-muted-foreground w-14">
+                  {new Date(campaignItem.createdAt).toLocaleTimeString([], {
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    hour12: true,
+                  })}
+                </p>
 
-              <Popover>
-                <PopoverTrigger asChild>
-                  <Button variant="outline"> ... </Button>
-                </PopoverTrigger>
-                <PopoverContent>
-                  <div className="grid gap-4">
-                    <Button asChild variant={"outline"}>
-                      <Link href={"/dashboard/campaign/123"}>Edit Campaign</Link>
-                    </Button>
-                  </div>
-                </PopoverContent>
-              </Popover>
-
-            </div>
-          </CardContent>
-        </Card>
-      ))}
+                <Popover>
+                  <PopoverTrigger asChild>
+                    <Button variant="outline"> ... </Button>
+                  </PopoverTrigger>
+                  <PopoverContent>
+                    <div className="grid gap-4">
+                      <Button asChild variant={"outline"}>
+                        <Link href={"/dashboard/campaign/123"}>
+                          Edit Campaign
+                        </Link>
+                      </Button>
+                    </div>
+                  </PopoverContent>
+                </Popover>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
     </div>
   );
 }
--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -62,14 +62,14 @@ export default function Page() {
   };
 
   useEffect(() => {
-    activeCampaignId && console.log(activeCampaignId);
+    console.log(activeCampaignId);
   }, [activeCampaignId]);
 
   return (
     <ScrollArea className="h-full">
+      <span>{activeCampaignId}</span>
       <div className="flex-1 space-y-4">
         <div className="flex items-center justify-between space-y-2">
-
           {/* <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2> */}
           {/* <Button onClick={() => handleOpenSidebar("123")}>Open Sidebar</Button> */}
 
@@ -108,7 +108,6 @@ export default function Page() {
                 >
                   <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
                 </svg> */}
-
               </CardHeader>
               <CardContent>
                 <div className="text-2xl font-bold">{card.value}</div>
@@ -224,12 +223,10 @@ export default function Page() {
               <RecentSales />
             </CardContent>
           </Card> */}
-          
         </div>
 
         {/* </TabsContent> */}
         {/* </Tabs> */}
-
       </div>
     </ScrollArea>
   );
