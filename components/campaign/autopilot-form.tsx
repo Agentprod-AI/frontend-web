d4227a54e243eec54359626bff6a616aa3775150
Adarsh <hydrogenproton1@gmail.com>
2024-03-26 12:53:16 +0530

    fixed build errors

--- components/campaign/autopilot-form.tsx
+++ components/campaign/autopilot-form.tsx
@@ -1,170 +1,170 @@
-// "use client";
+"use client";
 
-// import Link from "next/link";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useForm } from "react-hook-form";
-// import { z } from "zod";
+import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-// import { Button } from "@/components/ui/button";
-// import { Checkbox } from "@/components/ui/checkbox";
-// import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-// import { Switch } from "@/components/ui/switch";
-// import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Switch } from "@/components/ui/switch";
+import { toast } from "@/components/ui/use-toast";
 
-// const autopilotFormSchema = z.object({
-//   all_messages_actions: z.boolean().default(false).optional(),
-//   outbound_sequences: z.boolean().default(false).optional(),
-//   replies: z.boolean().default(false).optional(),
-//   out_of_office: z.boolean().default(false).optional(),
-//   positive: z.boolean().default(false).optional(),
-//   negative: z.boolean().default(false).optional(),
-//   neutral: z.boolean().default(false).optional(),
-//   maybe_later: z.boolean().default(false).optional(),
-//   forwarded: z.boolean().default(false).optional(),
-//   error: z.boolean().default(false).optional(),
-// });
+const autopilotFormSchema = z.object({
+  all_messages_actions: z.boolean().default(false).optional(),
+  outbound_sequences: z.boolean().default(false).optional(),
+  replies: z.boolean().default(false).optional(),
+  out_of_office: z.boolean().default(false).optional(),
+  positive: z.boolean().default(false).optional(),
+  negative: z.boolean().default(false).optional(),
+  neutral: z.boolean().default(false).optional(),
+  maybe_later: z.boolean().default(false).optional(),
+  forwarded: z.boolean().default(false).optional(),
+  error: z.boolean().default(false).optional(),
+});
 
-// type AutopilotFormValues = z.infer<typeof autopilotFormSchema>;
+type AutopilotFormValues = z.infer<typeof autopilotFormSchema>;
 
-// interface autopilotOptionsInterface {
-//   name:
-//     | "all_messages_actions"
-//     | "outbound_sequences"
-//     | "replies"
-//     | "out_of_office"
-//     | "positive"
-//     | "negative"
-//     | "neutral"
-//     | "maybe_later"
-//     | "forwarded"
-//     | "error";
-//   label: string;
-//   description: string;
-// }
-// const autopilotOptions: autopilotOptionsInterface[] = [
-//   {
-//     name: "all_messages_actions",
-//     label: "All messages and actions",
-//     description: "Turn on autopilot for all messages and actions.",
-//   },
-//   {
-//     name: "outbound_sequences",
-//     label: "Outbound sequences",
-//     description: "First contact and follow-ups as part of a campaign sequence.",
-//   },
-//   {
-//     name: "replies",
-//     label: "Replies",
-//     description: "Responses and actions to inbound replies.",
-//   },
-//   {
-//     name: "out_of_office",
-//     label: "Out of office",
-//     description: "Follow-up when they are back at work.",
-//   },
-//   {
-//     name: "positive",
-//     label: "Positive",
-//     description: "Respond towards campaign goal on positive reply.",
-//   },
-//   {
-//     name: "negative",
-//     label: "Negative",
-//     description: "Respond, mark as lost, and block contact on negative reply.",
-//   },
-//   {
-//     name: "neutral",
-//     label: "Neutral",
-//     description: "Respond towards campaign goal on neutral reply.",
-//   },
-//   {
-//     name: "maybe_later",
-//     label: "Maybe later",
-//     description: "Respond and follow-up later towards campaign goal.",
-//   },
-//   {
-//     name: "forwarded",
-//     label: "Forwarded",
-//     description: "Start a new conversation on forwarded reply.",
-//   },
-//   {
-//     name: "error",
-//     label: "Error",
-//     description: "Block contact and mark as lost if an email bounces.",
-//   },
-// ];
+interface autopilotOptionsInterface {
+  name:
+    | "all_messages_actions"
+    | "outbound_sequences"
+    | "replies"
+    | "out_of_office"
+    | "positive"
+    | "negative"
+    | "neutral"
+    | "maybe_later"
+    | "forwarded"
+    | "error";
+  label: string;
+  description: string;
+}
+const autopilotOptions: autopilotOptionsInterface[] = [
+  {
+    name: "all_messages_actions",
+    label: "All messages and actions",
+    description: "Turn on autopilot for all messages and actions.",
+  },
+  {
+    name: "outbound_sequences",
+    label: "Outbound sequences",
+    description: "First contact and follow-ups as part of a campaign sequence.",
+  },
+  {
+    name: "replies",
+    label: "Replies",
+    description: "Responses and actions to inbound replies.",
+  },
+  {
+    name: "out_of_office",
+    label: "Out of office",
+    description: "Follow-up when they are back at work.",
+  },
+  {
+    name: "positive",
+    label: "Positive",
+    description: "Respond towards campaign goal on positive reply.",
+  },
+  {
+    name: "negative",
+    label: "Negative",
+    description: "Respond, mark as lost, and block contact on negative reply.",
+  },
+  {
+    name: "neutral",
+    label: "Neutral",
+    description: "Respond towards campaign goal on neutral reply.",
+  },
+  {
+    name: "maybe_later",
+    label: "Maybe later",
+    description: "Respond and follow-up later towards campaign goal.",
+  },
+  {
+    name: "forwarded",
+    label: "Forwarded",
+    description: "Start a new conversation on forwarded reply.",
+  },
+  {
+    name: "error",
+    label: "Error",
+    description: "Block contact and mark as lost if an email bounces.",
+  },
+];
 
-// // This can come from your database or API.
-// const defaultValues: Partial<AutopilotFormValues> = {
-//   all_messages_actions: false,
-//   outbound_sequences: false,
-//   replies: false,
-//   out_of_office: false,
-//   positive: false,
-//   negative: false,
-//   neutral: false,
-//   maybe_later: false,
-//   forwarded: false,
-//   error: false,
-// };
-// export function AutopilotForm() {
-//   const form = useForm<AutopilotFormValues>({
-//     resolver: zodResolver(autopilotFormSchema),
-//     defaultValues,
-//   });
+// This can come from your database or API.
+const defaultValues: Partial<AutopilotFormValues> = {
+  all_messages_actions: false,
+  outbound_sequences: false,
+  replies: false,
+  out_of_office: false,
+  positive: false,
+  negative: false,
+  neutral: false,
+  maybe_later: false,
+  forwarded: false,
+  error: false,
+};
+export function AutopilotForm() {
+  const form = useForm<AutopilotFormValues>({
+    resolver: zodResolver(autopilotFormSchema),
+    defaultValues,
+  });
 
-//   function onSubmit(data: AutopilotFormValues) {
-//     console.log("Data: ", data);
-//     toast({
-//       title: "You submitted the following values:",
-//       description: (
-//         <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-//           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-//         </pre>
-//       ),
-//     });
-//   }
+  function onSubmit(data: AutopilotFormValues) {
+    console.log("Data: ", data);
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
 
-//   return (
-//     <Form {...form}>
-//       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-//         <div>
-//           {/* <h3 className="mb-4 text-lg font-medium">Email Notifications</h3> */}
-//           <div className="space-y-2">
-//             {autopilotOptions.map((val, ind) => (
-//               <FormField
-//                 key={`autopilot_${ind}`}
-//                 control={form.control}
-//                 name={val.name}
-//                 render={({ field }) => (
-//                   <FormItem className="flex flex-row items-center justify-between rounded-lg px-4 py-2">
-//                     <div className="space-y-0.5">
-//                       <FormLabel className="text-base">{val.label}</FormLabel>
-//                       <FormDescription>{val.description}</FormDescription>
-//                     </div>
-//                     <FormControl>
-//                       <Switch
-//                         checked={field.value}
-//                         onCheckedChange={field.onChange}
-//                       />
-//                     </FormControl>
-//                   </FormItem>
-//                 )}
-//               />
-//             ))}
-//           </div>
-//         </div>
-//         <Button type="submit">Update notifications</Button>
-//       </form>
-//     </Form>
-//   );
-// }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <div>
+          {/* <h3 className="mb-4 text-lg font-medium">Email Notifications</h3> */}
+          <div className="space-y-2">
+            {autopilotOptions.map((val, ind) => (
+              <FormField
+                key={`autopilot_${ind}`}
+                control={form.control}
+                name={val.name}
+                render={({ field }) => (
+                  <FormItem className="flex flex-row items-center justify-between rounded-lg px-4 py-2">
+                    <div className="space-y-0.5">
+                      <FormLabel className="text-base">{val.label}</FormLabel>
+                      <FormDescription>{val.description}</FormDescription>
+                    </div>
+                    <FormControl>
+                      <Switch
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+            ))}
+          </div>
+        </div>
+        <Button type="submit">Update notifications</Button>
+      </form>
+    </Form>
+  );
+}
--- components/campaign/offering-form.tsx
+++ components/campaign/offering-form.tsx
@@ -1,172 +1,172 @@
-// "use client";
+"use client";
 
-// // import Link from "next/link";
-// import { zodResolver } from "@hookform/resolvers/zod";
-// import { useFieldArray, useForm } from "react-hook-form";
-// import { z } from "zod";
-// // import { cn } from "@/lib/utils";
-// import { Button } from "@/components/ui/button";
+// import Link from "next/link";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useFieldArray, useForm } from "react-hook-form";
+import { z } from "zod";
+// import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 // import {
-//   Form,
-//   FormControl,
-//   FormDescription,
-//   FormField,
-//   FormItem,
-//   FormLabel,
-//   FormMessage,
-// } from "@/components/ui/form";
-// import { Input } from "@/components/ui/input";
-// // import {
-// //   Select,
-// //   SelectContent,
-// //   SelectItem,
-// //   SelectTrigger,
-// //   SelectValue,
-// // } from "@/components/ui/select";
-// import { Textarea } from "@/components/ui/textarea";
-// import { toast } from "sonner";
+//   Select,
+//   SelectContent,
+//   SelectItem,
+//   SelectTrigger,
+//   SelectValue,
+// } from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "sonner";
 
-// const profileFormSchema = z.object({
-//   product_offering: z
-//     .string()
-//     .min(2, {
-//       message: "Product offering must be at least 2 characters.",
-//     })
-//     .max(30, {
-//       message: "Product offering must not be longer than 30 characters.",
-//     }),
-//   offering_details: z.string().max(160).min(4),
-// });
+const profileFormSchema = z.object({
+  product_offering: z
+    .string()
+    .min(2, {
+      message: "Product offering must be at least 2 characters.",
+    })
+    .max(30, {
+      message: "Product offering must not be longer than 30 characters.",
+    }),
+  offering_details: z.string().max(160).min(4),
+});
 
-// type ProfileFormValues = z.infer<typeof profileFormSchema>;
+type ProfileFormValues = z.infer<typeof profileFormSchema>;
 
-// // This can come from your database or API.
-// const defaultValues: Partial<ProfileFormValues> = {
-//   product_offering: "",
-//   offering_details: "",
-// };
+// This can come from your database or API.
+const defaultValues: Partial<ProfileFormValues> = {
+  product_offering: "",
+  offering_details: "",
+};
 
-// export function OfferingForm() {
-//   const form = useForm<ProfileFormValues>({
-//     resolver: zodResolver(profileFormSchema),
-//     defaultValues,
-//     mode: "onChange",
-//   });
+export function OfferingForm() {
+  const form = useForm<ProfileFormValues>({
+    resolver: zodResolver(profileFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
 
-//   //   const { fields, append } = useFieldArray({
-//   //     name: "urls",
-//   //     control: form.control,
-//   //   });
+  //   const { fields, append } = useFieldArray({
+  //     name: "urls",
+  //     control: form.control,
+  //   });
 
-//   function onSubmit(data: ProfileFormValues) {
-//     console.log("form submitted", data);
-//     toast("You submitted the following values:", {
-//       description: (
-//         <pre className="mt-2 w-[320px] rounded-md bg-slate-950 p-4">
-//           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-//         </pre>
-//       ),
-//       // `Product Name: ${data.product_offering} Product Details: ${data.offering_details}`,
-//     });
-//   }
+  function onSubmit(data: ProfileFormValues) {
+    console.log("form submitted", data);
+    toast("You submitted the following values:", {
+      description: (
+        <pre className="mt-2 w-[320px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+      // `Product Name: ${data.product_offering} Product Details: ${data.offering_details}`,
+    });
+  }
 
-//   return (
-//     <Form {...form}>
-//       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-//         <FormField
-//           control={form.control}
-//           name="product_offering"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Product Offering</FormLabel>
-//               <FormControl>
-//                 <Input placeholder="Product" {...field} />
-//               </FormControl>
-//               <FormDescription>This is your product name</FormDescription>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         {/* <FormField
-//           control={form.control}
-//           name="email"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Email</FormLabel>
-//               <Select onValueChange={field.onChange} defaultValue={field.value}>
-//                 <FormControl>
-//                   <SelectTrigger>
-//                     <SelectValue placeholder="Select a verified email to display" />
-//                   </SelectTrigger>
-//                 </FormControl>
-//                 <SelectContent>
-//                   <SelectItem value="m@example.com">m@example.com</SelectItem>
-//                   <SelectItem value="m@google.com">m@google.com</SelectItem>
-//                   <SelectItem value="m@support.com">m@support.com</SelectItem>
-//                 </SelectContent>
-//               </Select>
-//               <FormDescription>
-//                 You can manage verified email addresses in your{" "}
-//                 <Link href="/examples/forms">email settings</Link>.
-//               </FormDescription>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         /> */}
-//         <FormField
-//           control={form.control}
-//           name="offering_details"
-//           render={({ field }) => (
-//             <FormItem>
-//               <FormLabel>Details of offers and details</FormLabel>
-//               <FormControl>
-//                 <Textarea
-//                   placeholder="Describe the product and features."
-//                   // className="resize-none"
-//                   {...field}
-//                 />
-//               </FormControl>
-//               <FormDescription>
-//                 You can write details of your product here
-//               </FormDescription>
-//               <FormMessage />
-//             </FormItem>
-//           )}
-//         />
-//         {/* <div>
-//           {fields.map((field, index) => (
-//             <FormField
-//               control={form.control}
-//               key={field.id}
-//               name={`urls.${index}.value`}
-//               render={({ field }) => (
-//                 <FormItem>
-//                   <FormLabel className={cn(index !== 0 && "sr-only")}>
-//                     URLs
-//                   </FormLabel>
-//                   <FormDescription className={cn(index !== 0 && "sr-only")}>
-//                     Add links to your website, blog, or social media profiles.
-//                   </FormDescription>
-//                   <FormControl>
-//                     <Input {...field} />
-//                   </FormControl>
-//                   <FormMessage />
-//                 </FormItem>
-//               )}
-//             />
-//           ))}
-//           <Button
-//             type="button"
-//             variant="outline"
-//             size="sm"
-//             className="mt-2"
-//             onClick={() => append({ value: "" })}
-//           >
-//             Add URL
-//           </Button>
-//         </div> */}
-//         <Button type="submit">Create Offer</Button>
-//       </form>
-//     </Form>
-//   );
-// }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="product_offering"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product Offering</FormLabel>
+              <FormControl>
+                <Input placeholder="Product" {...field} />
+              </FormControl>
+              <FormDescription>This is your product name</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <Select onValueChange={field.onChange} defaultValue={field.value}>
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select a verified email to display" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  <SelectItem value="m@example.com">m@example.com</SelectItem>
+                  <SelectItem value="m@google.com">m@google.com</SelectItem>
+                  <SelectItem value="m@support.com">m@support.com</SelectItem>
+                </SelectContent>
+              </Select>
+              <FormDescription>
+                You can manage verified email addresses in your{" "}
+                <Link href="/examples/forms">email settings</Link>.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
+        <FormField
+          control={form.control}
+          name="offering_details"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Details of offers and details</FormLabel>
+              <FormControl>
+                <Textarea
+                  placeholder="Describe the product and features."
+                  // className="resize-none"
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription>
+                You can write details of your product here
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* <div>
+          {fields.map((field, index) => (
+            <FormField
+              control={form.control}
+              key={field.id}
+              name={`urls.${index}.value`}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel className={cn(index !== 0 && "sr-only")}>
+                    URLs
+                  </FormLabel>
+                  <FormDescription className={cn(index !== 0 && "sr-only")}>
+                    Add links to your website, blog, or social media profiles.
+                  </FormDescription>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          ))}
+          <Button
+            type="button"
+            variant="outline"
+            size="sm"
+            className="mt-2"
+            onClick={() => append({ value: "" })}
+          >
+            Add URL
+          </Button>
+        </div> */}
+        <Button type="submit">Create Offer</Button>
+      </form>
+    </Form>
+  );
+}
