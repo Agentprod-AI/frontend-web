5242d4377ce50047fc9d13bedcd188fb98c0594a
Sanika <sanikasuryawanshi0305@gmail.com>
2024-03-28 12:09:20 +0530

    Modify styling

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -23,12 +23,12 @@ interface NavProps {
 }
 
 export function Nav({ links, isCollapsed }: NavProps) {
-  const path = usePathname();
+  const path = usePathname(); 
 
   return (
     <div
       data-collapsed={isCollapsed}
-      className="md:pt-16 group flex-col gap-4 flex"
+      className="group flex-col gap-4 flex"
     >
       <nav className="grid gap-1 px-2 pt-2 group-[[data-collapsed=true]]:justify-center group-[[data-collapsed=true]]:px-2">
         {/* <Separator /> */}
@@ -36,7 +36,7 @@ export function Nav({ links, isCollapsed }: NavProps) {
           return (
             <div key={index}>
               {nav.items.map((link, index) => {
-                const Icon = Icons[link.icon || "arrowRight"];
+                const Icon = link.icon && Icons[link.icon] ? Icons[link.icon] : null;
                 return isCollapsed ? (
                   <Tooltip key={index} delayDuration={0}>
                     <TooltipTrigger asChild>
@@ -51,14 +51,14 @@ export function Nav({ links, isCollapsed }: NavProps) {
                       >
                         <span
                           className={cn(
-                            "group flex items-center justify-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+                            "group flex items-center rounded-md px-3 py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
                             path === link.href
                               ? "bg-primary text-accent hover:bg-primary hover:text-accent"
                               : "transparent",
                             link.disabled && "cursor-not-allowed opacity-80"
-                          )}
+                            )}
                         >
-                          <Icon className="h-4 w-4" />
+                          {Icon && <Icon className="h-4 w-4 mr-2" />}
                           <span className="sr-only">{link.title}</span>
                         </span>
                       </Link>
@@ -93,9 +93,9 @@ export function Nav({ links, isCollapsed }: NavProps) {
                           ? "bg-primary text-accent hover:bg-primary hover:text-accent"
                           : "transparent",
                         link.disabled && "cursor-not-allowed opacity-80"
-                      )}
+                        )}
                     >
-                      <Icon className="mr-2 h-4 w-4" />
+                      {Icon && <Icon className="h-4 w-4 mr-2" />}
                       {link.title}
                     </span>
                     {/* {link.label && (
