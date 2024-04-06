62b95dec165cd2c3e518b00795785c816ea47ad6
Sanika <sanikasuryawanshi0305@gmail.com>
2024-04-06 12:51:15 +0530

    Change employees to leads

--- app/(dashboard)/dashboard/leads/page.tsx
+++ app/(dashboard)/dashboard/leads/page.tsx
@@ -1,15 +1,15 @@
 import BreadCrumb from "@/components/breadcrumb";
-import { columns } from "@/components/tables/employee-tables/columns";
-import { EmployeeTable } from "@/components/tables/employee-tables/employee-table";
+import { columns } from "@/components/tables/lead-tables/columns";
+import { LeadTable } from "@/components/tables/lead-tables/lead-table";
 import { buttonVariants } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { Separator } from "@/components/ui/separator";
-import { Employee } from "@/constants/data";
+import { Lead } from "@/constants/data";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
 import Link from "next/link";
 
-const breadcrumbItems = [{ title: "Employee", link: "/dashboard/employee" }];
+const breadcrumbItems = [{ title: "Leads", link: "/dashboard/leads" }];
 
 type paramsProps = {
   searchParams: {
@@ -27,10 +27,10 @@ export default async function page({ searchParams }: paramsProps) {
     `https://api.slingacademy.com/v1/sample-data/users?offset=${offset}&limit=${pageLimit}` +
       (country ? `&search=${country}` : "")
   );
-  const employeeRes = await res.json();
-  const totalUsers = employeeRes.total_users; //1000
+  const leadRes = await res.json();
+  const totalUsers = leadRes.total_users; //1000
   const pageCount = Math.ceil(totalUsers / pageLimit);
-  const employee: Employee[] = employeeRes.users;
+  const lead: Lead[] = leadRes.users;
   return (
     <>
       <div className="flex-1 space-y-4  p-4 md:p-8 pt-6">
@@ -38,12 +38,12 @@ export default async function page({ searchParams }: paramsProps) {
 
         <div className="flex items-start justify-between">
           <Heading
-            title={`Employee (${totalUsers})`}
-            description="Manage employees (Server side table functionalities.)"
+            title={`Leads (${totalUsers})`}
+            description="Manage leads (Server side table functionalities.)"
           />
 
           <Link
-            href={"/dashboard/employee/new"}
+            href={"/dashboard/lead/new"}
             className={cn(buttonVariants({ variant: "default" }))}
           >
             <Plus className="mr-2 h-4 w-4" /> Add New
@@ -51,12 +51,12 @@ export default async function page({ searchParams }: paramsProps) {
         </div>
         <Separator />
 
-        <EmployeeTable
+        <LeadTable
           searchKey="country"
           pageNo={page}
           columns={columns}
           totalUsers={totalUsers}
-          data={employee}
+          data={lead}
           pageCount={pageCount}
         />
       </div>
