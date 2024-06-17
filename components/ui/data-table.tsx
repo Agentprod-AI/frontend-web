296f86ee21d2bd36205d4f03297d25b8c466bde2
Adarsh <hydrogenproton1@gmail.com>
2024-06-17 11:28:10 +0530

    fix: nuturing, revert to old logic in data-table.tsx

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -1,186 +1,3 @@
-// "use client";
-
-// import {
-//   ColumnDef,
-//   flexRender,
-//   getCoreRowModel,
-//   getFilteredRowModel,
-//   useReactTable,
-// } from "@tanstack/react-table";
-
-// import {
-//   Table,
-//   TableBody,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from "@/components/ui/table";
-// import { Input } from "./input";
-// import { Button } from "./button";
-// import { ScrollArea, ScrollBar } from "./scroll-area";
-
-// import { useState, useEffect } from "react";
-// import { Trash } from "lucide-react";
-// import { Contact, useLeads } from "@/context/lead-user";
-
-// interface DataTableProps<TData, TValue> {
-//   columns: ColumnDef<TData, TValue>[];
-//   data: TData[];
-//   searchKey: string;
-//   simple?: boolean;
-//   deleteAction?: boolean;
-// }
-
-// export function DataTable<TData, TValue>({
-//   columns,
-//   data,
-//   searchKey,
-//   simple,
-//   deleteAction,
-// }: DataTableProps<TData, TValue>) {
-//   const table = useReactTable({
-//     data,
-//     columns,
-//     getCoreRowModel: getCoreRowModel(),
-//     getFilteredRowModel: getFilteredRowModel(), // Enable filtering
-//     filterFns: {
-//       fuzzyText: fuzzyFilterFn, // Ensure this function is defined or imported
-//     },
-//   });
-
-//   const [isHovering, setIsHovering] = useState(false);
-//   const { existingLeads, setExistingLeads } = useLeads();
-
-//   /* this can be used to get the selectedrows
-//   console.log("value", table.getFilteredSelectedRowModel()); */
-
-//   useEffect(() => {
-//     const selectedRows = table.getFilteredSelectedRowModel().rows;
-//     const leads: Contact[] = selectedRows.map((row) => row.original as Contact);
-//     console.log("existing leads:", existingLeads);
-//     setExistingLeads(leads as Contact[]);
-//     // console.log("Selected rows:", selectedRows);
-//     console.log("selected leads:", leads);
-//   }, [table.getFilteredSelectedRowModel().rows]);
-
-//   function fuzzyFilterFn(rows: any, columnIds: any, filterValue: any) {
-//     return rows.filter((row: any) => {
-//       const rowValue = row.values[columnIds[0]];
-//       return rowValue !== undefined
-//         ? String(rowValue)
-//             .toLowerCase()
-//             .includes(String(filterValue).toLowerCase())
-//         : true;
-//     });
-//   }
-
-//   return (
-//     <>
-//       <Input
-//         placeholder={`Search ${searchKey}...`}
-//         value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
-//         onChange={(event) =>
-//           table.getColumn(searchKey)?.setFilterValue(event.target.value)
-//         }
-//         className="w-full md:max-w-sm my-3"
-//       />
-
-//       <ScrollArea className="rounded-md border h-[50vh]">
-//         <Table className="relative">
-//           <TableHeader>
-//             {table.getHeaderGroups().map((headerGroup) => (
-//               <TableRow key={headerGroup.id}>
-//                 {headerGroup.headers.map((header) => {
-//                   return (
-//                     <TableHead key={header.id}>
-//                       {header.isPlaceholder
-//                         ? null
-//                         : flexRender(
-//                             header.column.columnDef.header,
-//                             header.getContext()
-//                           )}
-//                     </TableHead>
-//                   );
-//                 })}
-//               </TableRow>
-//             ))}
-//           </TableHeader>
-//           <TableBody>
-//             {table.getRowModel().rows?.length ? (
-//               table.getRowModel().rows.map((row) => (
-//                 <TableRow
-//                   key={row.id}
-//                   data-state={row.getIsSelected() && "selected"}
-//                   onMouseEnter={() => setIsHovering(true)}
-//                   onMouseLeave={() => setIsHovering(false)}
-//                 >
-//                   {row.getVisibleCells().map((cell) => (
-//                     <TableCell key={cell.id}>
-//                       {flexRender(
-//                         cell.column.columnDef.cell,
-//                         cell.getContext()
-//                       )}
-//                     </TableCell>
-//                   ))}
-//                   {deleteAction && (
-//                     <TableCell>
-//                       <Button
-//                         variant={"ghost"}
-//                         className={`${
-//                           isHovering ? "visible" : "invisible"
-//                         } p-3  rounded-xl`}
-//                       >
-//                         <Trash className="h-4 w-4" />
-//                       </Button>
-//                     </TableCell>
-//                   )}
-//                 </TableRow>
-//               ))
-//             ) : (
-//               <TableRow>
-//                 <TableCell
-//                   colSpan={columns.length}
-//                   className="h-24 text-center"
-//                 >
-//                   No results.
-//                 </TableCell>
-//               </TableRow>
-//             )}
-//           </TableBody>
-//         </Table>
-//         <ScrollBar orientation="horizontal" />
-//       </ScrollArea>
-//       <div className="flex items-center justify-end space-x-2 py-4">
-//         {!simple && (
-//           <div className="flex-1 text-sm text-muted-foreground">
-//             {table.getFilteredSelectedRowModel().rows.length} of{" "}
-//             {table.getFilteredRowModel().rows.length} row(s) selected.
-//           </div>
-//         )}
-//         <div className="space-x-2">
-//           <Button
-//             variant="outline"
-//             size="sm"
-//             onClick={() => table.previousPage()}
-//             disabled={!table.getCanPreviousPage()}
-//           >
-//             Previous
-//           </Button>
-//           <Button
-//             variant="outline"
-//             size="sm"
-//             onClick={() => table.nextPage()}
-//             disabled={!table.getCanNextPage()}
-//           >
-//             Next
-//           </Button>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
 "use client";
 
 import {
@@ -190,6 +7,7 @@ import {
   getFilteredRowModel,
   useReactTable,
 } from "@tanstack/react-table";
+
 import {
   Table,
   TableBody,
@@ -201,8 +19,10 @@ import {
 import { Input } from "./input";
 import { Button } from "./button";
 import { ScrollArea, ScrollBar } from "./scroll-area";
-import { useState } from "react";
+
+import { useState, useEffect } from "react";
 import { Trash } from "lucide-react";
+import { Contact, useLeads } from "@/context/lead-user";
 
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
@@ -219,53 +39,62 @@ export function DataTable<TData, TValue>({
   simple,
   deleteAction,
 }: DataTableProps<TData, TValue>) {
-  const [searchValue, setSearchValue] = useState("");
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-    getFilteredRowModel: getFilteredRowModel(),
-    state: {
-      globalFilter: searchValue,
-    },
-    onGlobalFilterChange: setSearchValue,
-    manualFiltering: false,
   });
 
   const [isHovering, setIsHovering] = useState(false);
+  const { existingLeads, setExistingLeads } = useLeads();
+
+  /* this can be used to get the selectedrows 
+  console.log("value", table.getFilteredSelectedRowModel()); */
+
+  useEffect(() => {
+    const selectedRows = table.getFilteredSelectedRowModel().rows;
+    const leads: Contact[] = selectedRows.map((row) => row.original as Contact);
+    setExistingLeads(leads as Contact[]);
+    // console.log("Selected rows:", selectedRows);
+    console.log("selected leads:", leads);
+  }, [table.getFilteredSelectedRowModel().rows]);
 
   return (
     <>
       <Input
         placeholder={`Search ${searchKey}...`}
-        value={searchValue}
-        onChange={(event) => setSearchValue(event.target.value)}
+        value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
+        onChange={(event) =>
+          table.getColumn(searchKey)?.setFilterValue(event.target.value)
+        }
         className="w-full md:max-w-sm my-3"
       />
-
       <ScrollArea className="rounded-md border h-[50vh]">
         <Table className="relative">
           <TableHeader>
             {table.getHeaderGroups().map((headerGroup) => (
               <TableRow key={headerGroup.id}>
-                {headerGroup.headers.map((header) => (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                ))}
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
+                            header.column.columnDef.header,
+                            header.getContext()
+                          )}
+                    </TableHead>
+                  );
+                })}
               </TableRow>
             ))}
           </TableHeader>
           <TableBody>
-            {table.getRowModel().rows.length ? (
+            {table.getRowModel().rows?.length ? (
               table.getRowModel().rows.map((row) => (
                 <TableRow
                   key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
                   onMouseEnter={() => setIsHovering(true)}
                   onMouseLeave={() => setIsHovering(false)}
                 >
@@ -283,7 +112,7 @@ export function DataTable<TData, TValue>({
                         variant={"ghost"}
                         className={`${
                           isHovering ? "visible" : "invisible"
-                        } p-3 rounded-xl`}
+                        } p-3  rounded-xl`}
                       >
                         <Trash className="h-4 w-4" />
                       </Button>
@@ -308,7 +137,8 @@ export function DataTable<TData, TValue>({
       <div className="flex items-center justify-end space-x-2 py-4">
         {!simple && (
           <div className="flex-1 text-sm text-muted-foreground">
-            {table.getRowModel().rows.length} row(s) selected.
+            {table.getFilteredSelectedRowModel().rows.length} of{" "}
+            {table.getFilteredRowModel().rows.length} row(s) selected.
           </div>
         )}
         <div className="space-x-2">
