b6bdc7bed3cb77eae8cc137204b1ae6530874d14
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2024-07-09 19:28:16 +0530

    email

---
+++ app/(policy)/privacy/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className=" flex justify-center flex-col w-screen items-center container my-20">
+      <div className="text-6xl font-semibold bg-gradient-to-b from-foreground to-transparent bg-clip-text text-[10rem]  leading-none text-transparent">
+        Privacy Policy
+      </div>
+      <div className="mt-10">
+        <div>
+          <div className="font-medium">Effective Date: April 9, 2024</div>
+          <div>
+            This privacy policy governs the use of AgentProd (the "Service"). By
+            using the Service, you accept this privacy policy along with our
+            Terms of Service in full.
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
