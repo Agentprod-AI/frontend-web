1693bf757f3ecc71ada02cd2ee19ef09b2520187
Punyakrit Singh Makhni <punyakritsinghmakhni@gmail.com>
2024-07-11 13:12:08 +0530

    terms page

---
+++ app/(policy)/terms/page.tsx
@@ -0,0 +1,12 @@
+import Terms from "@/components/privacy/Terms";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Terms />
+    </div>
+  );
+}
+
+export default page;
---
+++ components/privacy/Terms.tsx
@@ -0,0 +1,235 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+
+function Terms() {
+  return (
+    <div className="h-screen overflow-y-scroll">
+      <div className="flex flex-col items-center  py-20 px-4">
+        <div className="text-6xl  font-semibold bg-gradient-to-b from-foreground to-transparent bg-clip-text leading-none text-transparent">
+          Terms of service
+        </div>
+        <div className="mt-16 text-white/80 max-w-4xl text-xs">
+          <div>
+            <div className="font-medium">Effective Date: April 9, 2024</div>
+            <div>
+              These terms and conditions govern the use of AgentProd (the
+              "Service"). By using the Service, you accept these terms and
+              conditions in full, along with our Privacy Policy.
+            </div>
+          </div>
+          <div className="my-8 leading-6 	">
+            <div className="text-xl font-semibold">1. Acceptance of Terms</div>
+            <div>
+              <div>
+                By accessing and using AgentProd, you agree to be bound by these
+                Terms of Service.
+              </div>
+            </div>
+          </div>
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">2. Service Description</div>
+            <div>
+              <div>
+                AgentProd provides an advanced AI-driven sales platform designed
+                to optimize and enhance your sales operations. Our service
+                includes a suite of tools for managing inbound, outbound, and
+                nurturing sales activities.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">
+              3. Suspension of Service
+            </div>
+            <div>
+              <div>
+                We reserve the right to suspend or terminate your access to the
+                Service at any time and for any reason. The AgentProd Beta Terms
+                will automatically terminate upon the release of a generally
+                available version of the AgentProd applications. You acknowledge
+                AgentProd is under no obligation to make the Service generally
+                available and may never do so. Further, should the Service
+                become generally available, you acknowledge that continued
+                access and use of the Service may be subject to your agreement
+                to pay additional fees. AgentProd reserves the right to modify
+                or terminate the Service or the AgentProd Beta Terms, and to
+                limit or deny access to the Service, at any time, in our sole
+                discretion, for any reason, with or without notice and without
+                liability to you.
+              </div>
+              <div>
+                You may discontinue your use of the Service at any time by
+                uninstalling the applicable application(s) and optionally
+                sending an email to info@agentprod.com requesting that your
+                AgentProd account be deleted.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">
+              4. Intellectual Property Rights
+            </div>
+            <div>
+              <div>
+                You agree that we own all legal rights, title, and interest in
+                and to the Service, including all intellectual property rights,
+                and except for the license provided herein, no other rights or
+                permissions to the Service are granted. All generated text by
+                the Service is also owned by us.
+              </div>
+              <div>
+                Nothing herein gives you a right to use any of our trade names,
+                trademarks, service marks, logos, domain names, and other
+                distinctive brand features. Except to the extent permitted by
+                law, you may not modify, distribute, prepare derivative works
+                of, reverse engineer, reverse assemble, disassemble, decompile,
+                or otherwise attempt to decipher any code in connection with the
+                Service and/or any other aspect of AgentProd technology, except
+                as permitted by us.
+              </div>
+              <div>
+                You hereby grant us an irrevocable, non-exclusive, royalty-free,
+                worldwide license to use, reproduce, publish, adjust, and
+                display your logo or trademarks in connection to your use of our
+                services. This includes, but is not limited to, use in marketing
+                materials, advertisements, and on our website to illustrate that
+                you are a customer of our service. This permission begins from
+                the moment you sign up for the service as a free trial customer,
+                paying customer, or on the waiting list.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">5. Governing Law</div>
+            <div>
+              <div>
+                This agreement will be governed by and construed in accordance
+                with the laws of the State of Delaware, without regard to its
+                conflict of laws provisions. Any disputes arising under these
+                terms shall be resolved in the courts of Delaware.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">6. Limits on Liability</div>
+            <div>
+              <div>
+                In no event shall AgentProd or its affiliates be liable for any
+                indirect, special, consequential, or incidental loss, exemplary
+                or other damages related to these terms whether direct or
+                indirect, however caused and based on any theory of liability,
+                and whether or not for breach of contract, tort (including
+                negligence), violation of statute, or otherwise, and whether or
+                not AgentProd has been advised of the possibility of such
+                damages. To the extent permitted by applicable law, AgentProd’s
+                maximum liability hereunder is limited to $10.
+              </div>
+              <div>
+                Some jurisdictions do not allow limitation or exclusion of
+                liability for incidental or consequential damages, so some of
+                the above limitations may not apply to you.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">7. Compliance with Laws</div>
+            <div>
+              <div>
+                You agree to comply with the CAN-SPAM Act of 2003, the European
+                Union's ePrivacy Directive, and all other applicable laws and
+                regulations. You agree not to use the Service to send emails
+                that are misleading, deceptive, or fraudulent, or that violate
+                the rights of others. If the laws of your country or
+                jurisdiction require you to obtain consent from your recipients
+                before sending them commercial email, you agree to do so before
+                using the Service to send such emails. You agree to comply with
+                all applicable laws and regulations in connection with your use
+                of the Service, and that AgentProd is not responsible for your
+                compliance with such laws and regulations. You agree to
+                indemnify and hold harmless AgentProd from any and all claims,
+                damages, liabilities, costs, and expenses (including reasonable
+                attorneys' fees) arising from or related to your use of the
+                Service.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">8. Warranty Disclaimer</div>
+            <div>
+              <div>
+                You hereby acknowledge and agree that the Service is provided by
+                AgentProd on an “as is” and “as available” basis, and your
+                access to and/or use of the Service is at your sole risk. To the
+                extent permitted by applicable law, AgentProd expressly
+                disclaims all warranties and conditions of any kind, whether
+                express or implied, including, but not limited to, those of
+                merchantability, satisfactory quality, title, fitness for a
+                particular purpose, and non-infringement. AgentProd makes no
+                warranty that any of the services will meet your requirements
+                and/or that the services will be uninterrupted, timely,
+                error-free, or secure. You acknowledge and agree that the
+                Service is not in scope for an SOC 2 type II and other
+                independent security audits and security certifications. Some
+                jurisdictions do not allow the exclusion of certain warranties
+                and conditions, so some of the above exclusions may not apply to
+                you.
+              </div>
+              <div>
+                You may send requests for technical support by emailing
+                support@agentprod.com.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">9. Payment Terms</div>
+            <div>
+              <div>
+                If applicable, users will be notified of subscription fees,
+                payment schedules, and refund policies. Payments are due at the
+                beginning of each billing cycle. Failure to make timely payments
+                may result in suspension or termination of your account.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">10. Changes to Terms</div>
+            <div>
+              <div>
+                We may update these terms periodically. Users will be notified
+                of significant changes, and continued use of the Service
+                constitutes acceptance of the new terms.
+              </div>
+            </div>
+          </div>
+
+          <div className="my-8 leading-6">
+            <div className="text-xl font-semibold">Contact Information</div>
+            <div>
+              <div>
+                If you have any questions or concerns about these Terms of
+                Service, please contact us at: AgentProd
+              </div>
+              <div
+                className="cursor-pointer"
+                onClick={() => window.open("mailto:info@agentprod.com")}
+              >
+                <span className="font-semibold">Email</span>: info@agentprod.com
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Terms;
