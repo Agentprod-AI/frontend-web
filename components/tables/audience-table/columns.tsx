2652f6b9f0d9cb12fc6e91b4522b77dbc26c4f40
md-anas-sabah <mdanassabah@gmail.com>
2024-07-13 19:22:17 +0530

    Added useCallback,Company Name in Lead, ThreadTime

--- components/tables/audience-table/columns.tsx
+++ components/tables/audience-table/columns.tsx
@@ -1,121 +1,7 @@
-// "use client";
-// import { ColumnDef } from "@tanstack/react-table";
-// import { CellAction } from "./cell-action";
-// import { User } from "@/constants/data";
-// import { NameAction } from "./name-action";
-// import { Lead, Organization } from "@/context/lead-user";
-// import { Checkbox } from "@/components/ui/checkbox";
-
-// export const leadColumns: ColumnDef<Lead>[] = [
-//   {
-//     accessorKey: "photo_url",
-//     header: "AVATAR",
-//     cell: ({ row }) => (
-//       <img
-//         src={row.original.photo_url}
-//         alt="Avatar"
-//         style={{ width: "40px", height: "40px", borderRadius: "50%" }}
-//       />
-//     ),
-//   },
-//   {
-//     accessorKey: "name",
-//     header: "NAME",
-//     cell: ({ row }) => <NameAction data={row.original} />,
-//   },
-//   {
-//     accessorKey: "title",
-//     header: "ROLE",
-//   },
-//   {
-//     accessorFn: (row) =>
-//       row.employment_history?.[0]?.organization_name || "N/A",
-//     header: "COMPANY",
-//   },
-// ];
-
-// // export const orgColumns: ColumnDef<Organization>[] = [
-// //   {
-// // accessorKey: "name",
-// //     header: "NAME",
-// //     cell: ({ row }) => <NameAction data={row.original} />,
-// //   },
-// //   {
-// //     accessorKey: "title",
-// //     header: "DOMAIN",
-// //   },
-// //   {
-// //     id: "actions",
-// //     cell: ({ row }) => <CellAction data={row.original} />,
-// //   },
-// // ];
-
-// export const contactsColumn: ColumnDef<Lead>[] = [
-//   {
-//     accessorKey: "name",
-//     header: "NAME",
-//     cell: ({ row }) => <NameAction data={row.original} />,
-//   },
-//   {
-//     accessorKey: "title",
-//     header: "ROLE",
-//   },
-//   {
-//     accessorKey: "last_replied",
-//     header: "RESPONDED",
-//   },
-//   {
-//     accessorKey: "last_contacted",
-//     header: "LAST CONTACTED",
-//   },
-// ];
-
-// export const selectContactsColumn: ColumnDef<Lead>[] = [
-//   {
-//     id: "select",
-//     header: ({ table }) => (
-//       <Checkbox
-//         checked={
-//           table.getIsAllPageRowsSelected() ||
-//           (table.getIsSomePageRowsSelected() && "indeterminate")
-//         }
-//         onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-//         aria-label="Select all"
-//       />
-//     ),
-//     cell: ({ row }) => (
-//       <Checkbox
-//         checked={row.getIsSelected()}
-//         onCheckedChange={(value) => row.toggleSelected(!!value)}
-//         aria-label="Select row"
-//       />
-//     ),
-//   },
-//   {
-//     accessorKey: "name",
-//     header: "NAME",
-//     cell: ({ row }) => <NameAction data={row.original} />,
-//   },
-//   {
-//     accessorKey: "title",
-//     header: "ROLE",
-//   },
-//   {
-//     accessorKey: "responded",
-//     header: "RESPONDED",
-//   },
-//   {
-//     accessorKey: "lastContacted",
-//     header: "LAST CONTACTED",
-//   },
-// ];
-
 "use client";
 import { ColumnDef } from "@tanstack/react-table";
-import { CellAction } from "./cell-action";
-import { User } from "@/constants/data";
 import { NameAction } from "./name-action";
-import { Lead, Organization } from "@/context/lead-user";
+import { Lead } from "@/context/lead-user";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Trash } from "lucide-react";
 import { Button } from "@/components/ui/button";
@@ -147,7 +33,9 @@ export const leadColumns: ColumnDef<Lead>[] = [
       <Image
         src={row.original.photo_url}
         alt="Avatar"
-        style={{ width: "40px", height: "40px", borderRadius: "50%" }}
+        width={40}
+        height={40}
+        style={{ borderRadius: "50%" }}
       />
     ),
   },
@@ -164,6 +52,11 @@ export const leadColumns: ColumnDef<Lead>[] = [
     accessorFn: (row) =>
       row.employment_history?.[0]?.organization_name || "N/A",
     header: "COMPANY",
+    cell: ({ row }) => {
+      const companyName =
+        row.original.employment_history?.[0]?.organization_name || "N/A";
+      return <span>{companyName}</span>;
+    },
   },
 ];
 
@@ -176,6 +69,16 @@ export const contactsColumn: ColumnDef<Lead>[] = [
   {
     accessorKey: "title",
     header: "ROLE",
+  },
+  {
+    accessorFn: (row) =>
+      row.employment_history?.[0]?.organization_name || "N/A",
+    header: "COMPANY",
+    cell: ({ row }) => {
+      const companyName =
+        row.original.employment_history?.[0]?.organization_name || "N/A";
+      return <span>{companyName}</span>;
+    },
   },
   {
     accessorKey: "last_replied",
@@ -216,6 +119,16 @@ export const selectContactsColumn: ColumnDef<Lead>[] = [
   {
     accessorKey: "title",
     header: "ROLE",
+  },
+  {
+    accessorFn: (row) =>
+      row.employment_history?.[0]?.organization_name || "N/A",
+    header: "COMPANY",
+    cell: ({ row }) => {
+      const companyName =
+        row.original.employment_history?.[0]?.organization_name || "N/A";
+      return <span>{companyName}</span>;
+    },
   },
   {
     accessorKey: "responded",
